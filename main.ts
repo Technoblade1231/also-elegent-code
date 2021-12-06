1e70b268dcaf9e136872d7fb7c34fd5c1fe385aa
Technoblade1231 <92799244+Technoblade1231@users.noreply.github.com>
2021-12-06 08:55:00 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        images.createBigImage(`
+            . . . . . . . . . .
+            . . . . . . # # # .
+            . . # . . . # . # .
+            . . . . . . # # # .
+            . . . . . . . . . .
+            `).scrollImage(5, 200)
+        images.createBigImage(`
+            # # # # # . . . . .
+            # . . . # . . . . .
+            # . . . # . . . . .
+            # . . . # . . . . .
+            # # # # # . . . . .
+            `).scrollImage(5, 200)
+    }
+})
+basic.forever(function () {
+	
+})
